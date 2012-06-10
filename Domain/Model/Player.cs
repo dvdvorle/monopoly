11f78482b4e83fd6057714d62ba64c9a49dd0d2c
Dominique van de Vorle <dvdvorle@gmail.com>
2012-06-10 16:53:27 +0200

    Model stubs added.

---
+++ Domain/Model/Player.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Player.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Restless.Monopoly.Domain.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Player
+    {
+    }
+}
