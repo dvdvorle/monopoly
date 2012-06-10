7dfb1441fe56ad209ad6403d47da4d94767f5ccb
Dominique van de Vorle <dvdvorle@gmail.com>
2012-06-10 17:37:06 +0200

    Game.AddPlayer added.

--- Domain/Model/Game.cs
+++ Domain/Model/Game.cs
@@ -16,11 +16,24 @@ namespace Restless.Monopoly.Domain.Model
     /// </summary>
     public class Game
     {
-        public IEnumerable<Player> Players { get; private set; }
+        public IEnumerable<Player> Players
+        {
+            get
+            {
+                return _players;
+            }
+        }
+
+        private List<Player> _players;
 
         public Game(List<Player> players)
         {
-            Players = players;
+            _players = players;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            _players.Add(player);
         }
     }
 }
