f0be64d8fffd0dc84cc699888d0e6c0b88f52cf9
Dominique van de Vorle <dvdvorle@gmail.com>
2012-06-11 14:58:43 +0200

    Webservice stubs added.

---
+++ Application/Model/PlayerDTO.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerDOT.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Restless.Monopoly.Application.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Runtime.Serialization;
+    
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [DataContract]
+    public class PlayerDTO
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
---
+++ Application/Webservice/IMonopoly.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IMonopoly.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Restless.Monopoly.Application.Webservice
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.ServiceModel;
+using Restless.Monopoly.Application.Model;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [ServiceContract]
+    public interface IMonopoly
+    {
+        IEnumerable<GameDTO> GetGamesList();
+        GameDTO CreateNewGame();
+        IEnumerable<PlayerDTO> GetPlayersIn(GameDTO game);
+    }
+}
